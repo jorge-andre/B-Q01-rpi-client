2a21e85f6d582c65378125109da304f4323a6d85
jorge-andre <jorge.matosandre@gmail.com>
2024-01-04 14:44:33 +0100

    Add setup for RGB led

--- B-Q01-Pi.py
+++ B-Q01-Pi.py
@@ -3,18 +3,55 @@ import RPi.GPIO as GPIO         # Import Raspberry Pi GPIO library
 from time import sleep          # Import the sleep function 
 
 GPIO.setmode(GPIO.BCM)            # Specify GPIO numbers instead of pin numbers
-led = 17
+rPin = 17
+gPin = 27
+bPin = 22
 
 GPIO.setwarnings(True)
-GPIO.setup(led, GPIO.OUT)
+GPIO.setup(rPin, GPIO.OUT)
+GPIO.setup(gPin, GPIO.OUT)
+GPIO.setup(bPin, GPIO.OUT)
+
+def turnOff():
+    GPIO.output(rPin, GPIO.LOW)
+    GPIO.output(gPin, GPIO.LOW)
+    GPIO.output(bPin, GPIO.LOW)
+
+def white():
+    GPIO.output(rPin, GPIO.HIGH)
+    GPIO.output(gPin, GPIO.HIGH)
+    GPIO.output(bPin, GPIO.HIGH)
+
+def red():
+    GPIO.output(rPin, GPIO.HIGH)
+    GPIO.output(gPin, GPIO.LOW)
+    GPIO.output(bPin, GPIO.LOW)
+
+def green():
+    GPIO.output(rPin, GPIO.LOW)
+    GPIO.output(gPin, GPIO.HIGH)
+    GPIO.output(bPin, GPIO.LOW)
+
+def blue():
+    GPIO.output(rPin, GPIO.LOW)
+    GPIO.output(gPin, GPIO.LOW)
+    GPIO.output(bPin, GPIO.HIGH)
 
 try:
     while True:
-        GPIO.output(led, GPIO.HIGH)
-        print("LED on")
-        sleep(1)
-        GPIO.output(led, GPIO.LOW)
+        turnOff()
         print("LED off")
         sleep(1)
+        red()
+        print("LED red")
+        sleep(1)
+        green()
+        print("LED green")
+        sleep(1)
+        blue()
+        print("LED blue")
+        sleep(1)
 finally:
+    turnOff()
     GPIO.cleanup()
+
