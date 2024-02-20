2afabd438f32008208bcdfc4fc91a63c33986fd1
Jorge André <jorge.matosandre@gmail.com>
2024-02-20 21:27:00 +0100

    Move from kafka-python to confluent-kafka

--- B-Q01-Pi.py
+++ B-Q01-Pi.py
@@ -1,16 +1,18 @@
 #!/user/bin/python
-import RPi.GPIO as GPIO         # Import Raspberry Pi GPIO library
-from time import sleep          # Import the sleep function 
+# import RPi.GPIO as GPIO
+from time import sleep
+from confluent_kafka import Consumer, KafkaException
+import json
 
-GPIO.setmode(GPIO.BCM)            # Specify GPIO numbers instead of pin numbers
+# GPIO.setmode(GPIO.BCM)            # Specify GPIO numbers instead of pin numbers
 rPin = 17
 gPin = 27
 bPin = 22
 
-GPIO.setwarnings(True)
-GPIO.setup(rPin, GPIO.OUT)
-GPIO.setup(gPin, GPIO.OUT)
-GPIO.setup(bPin, GPIO.OUT)
+# GPIO.setwarnings(True)
+# GPIO.setup(rPin, GPIO.OUT)
+# GPIO.setup(gPin, GPIO.OUT)
+# GPIO.setup(bPin, GPIO.OUT)
 
 def turnOff():
     GPIO.output(rPin, GPIO.LOW)
@@ -37,21 +39,41 @@ def blue():
     GPIO.output(gPin, GPIO.LOW)
     GPIO.output(bPin, GPIO.HIGH)
 
-try:
-    while True:
-        turnOff()
-        print("LED off")
-        sleep(1)
-        red()
-        print("LED red")
-        sleep(1)
-        green()
-        print("LED green")
-        sleep(1)
-        blue()
-        print("LED blue")
-        sleep(1)
-finally:
-    turnOff()
-    GPIO.cleanup()
 
+# consumer = KafkaConsumer('departures',
+#                          bootstrap_servers=['localhost:9092'],
+#                          client_id='rpi-zero-python',
+#                          value_deserializer=lambda m: json.loads(m.decode('utf8')))
+# while True:
+#     print('polling kafka')
+#     messages = consumer.poll()
+#     print(messages)
+#     for message in messages['departures']:
+#         print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+#                                               message.offset, message.key,
+#                                               message.value))
+#         arrivalTime = message.value["rtTime"]
+#         direction = message.value["direction"]
+#         print("time:%s  direction:%s" % (arrivalTime, direction))
+#         nextDeparture = message.value
+
+conf = {'bootstrap.servers': 'localhost:9092',
+        'group.id': 'foo',
+        'auto.offset.reset': 'smallest'}
+
+consumer = Consumer(conf)
+
+try:
+    consumer.subscribe(['departures'])
+
+    while True:
+        msg = consumer.poll()
+        if msg is None:
+            print('No messages to read.')
+        if msg.error():
+            raise KafkaException(msg.error)
+        
+        print(msg.value())
+        break
+finally:
+    consumer.close()
